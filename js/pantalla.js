0955d63f524e1d9a1aae67f178218d33cd2e692a
Oscar Cita <smalqs@gmail.com>
2022-02-18 10:17:33 -0500

    Funcionalidad completada

--- js/pantalla.js
+++ js/pantalla.js
@@ -1,17 +1,24 @@
 
 var pincel = canvas();
 pincel.fillStyle = 'white';
-pincel.fillRect(0,0,1200,800);
+pincel.fillRect(0,0,1200,700);
 
 var xAleatorio;
 var yAleatorio;
 
 var agregarPalabra = document.querySelector("#agregar-palabra");
-var listaDePalabras = ["Carajo"];
+var reiniciar = document.querySelector("#re-iniciar");
+var listaDePalabras = ["alura"]; //agregar en mayusculas
+var listaLetrasCorrectas = [];
+var validadordeCaracteres = [];
 var palabraSecreta="";
 var guiones="";
-var control = false;
+var control = 0;
 var contarmalo=1;
+var con=0;
+var cantidadMaxima= 0;
+
+
 
 function canvas (){
     var imagen = document.querySelector("canvas");
@@ -27,6 +34,7 @@ agregarPalabra.addEventListener("click",function(event){
     event.preventDefault();
     var formulario = document.querySelector("#formulario");
     var palabraNueva = formulario.palabraNueva.value;
+    palabraNueva = palabraNueva.toUpperCase();
     if (!palabraNueva==""){
         listaDePalabras.push(palabraNueva);
         console.log(listaDePalabras);
@@ -36,71 +44,106 @@ agregarPalabra.addEventListener("click",function(event){
     }
 })
 
-function nuevosElementos() {
-    // crear un nuevo elemento div
-    var nuevaDiv = document.createElement("h1");
-    // darle contenido
-    var nuevoContenido= document.createTextNode("Hola, cuanto tiempo!");
-    console.log(nuevoContenido);
-      //agregue el nodo de texto al div recién creado
-    nuevaDiv.appendChild(nuevoContenido); 
-  }
-
 /**Funcion al oprimir el boton INICIAR JUEGO
  * Me realizado el llamado de los guiones
  * y me genera la palabra Secreta
 */
 var iniciar = document.querySelector("#iniciar");
+var formulario = document.querySelector("#formulario");
 iniciar.addEventListener("click",function(){
-    controladorJuego();
+    control=1;
     palabraSecreta = (palabraAleatoria(listaDePalabras));
     guiones = palabraSecreta.replace(/./g, "_  ");
-    dibujar(pincel,guiones,800,200);
-    nuevosElementos();
-    
+    dibujar(pincel,"black",guiones,"55px arial",600,200);
+    iniciar.classList.add("invisible");
+    formulario.classList.add("invisible");
+
+    /**
+     * Se usa pala validar los caracteres de la palabra Secreta con la lista de
+     * letras correctas que ingresa el susuario
+     */
+    for(var i = 0 ; i < palabraSecreta.length ; i++){
+        if(!validadordeCaracteres.includes(palabraSecreta[i])){
+            validadordeCaracteres.push(palabraSecreta[i]);
+        }
+
+    }
 });
-
-
-
 
 /**Funcion que permite escoger una palabra aletoria */
 function palabraAleatoria(listado){
     var palabraSecreta = listado[Math.floor(Math.random()*listado.length)];
+    palabraSecreta=palabraSecreta.toUpperCase();
     return palabraSecreta;
+
 };
 
 document.addEventListener("keypress",function(evento){
-
-    if (control==true){
+    if (control==1){
+        cantidadMaxima=palabraSecreta.length;
         var letra = evento.key;
+        letra = letra.toUpperCase();
         console.log(letra);
-    
-        
-        if (palabraSecreta.includes(letra)){
-            
-            var contar = 0;
-            while (contar<palabraSecreta.length){
-                if(palabraSecreta[contar]===letra){
-                    var espacio = contar*39;
-                    var x = 798 +espacio;
-                    dibujar(pincel,letra,x,190);
-                }
-                contar++;   
-            }
-        }else{
-            vidas(pincel,contarmalo);
-            console.log(contarmalo +" intento");
-            contarmalo++;
+
+        if (!isNaN(letra)==true){
+            alert("¡Usted ingreso un número! debe ingresar letras.")
         }
         
-    }
-    
+        if (palabraSecreta.includes(letra) && !isNaN(letra)==false){
+            for(var i=0;i<palabraSecreta.length;i++ ){
+                if(palabraSecreta[i]==letra){
+                    var espacio = i*61;
+                    var x = 600 +espacio;
+                    dibujar(pincel,"blue",letra,"40px consolas",x,190);
+                    if(!listaLetrasCorrectas.includes(letra)){
+                        listaLetrasCorrectas.push(letra);
+                    }
+                }
+                
+            }
 
+        }else{
+            if(!isNaN(letra)==false){
+                if(contarmalo<8){
+                    vidas(pincel,contarmalo);
+                    var espacio = contarmalo*30;
+                    var x = 600 +espacio;
+                    dibujar(pincel,"red",letra,"35px verdana",x,400);
+                }    
+                contarmalo++;
+                if(contarmalo>=8){
+                    vidas(pincel,9);
+                    console.log("perdiste");
+                    control=0;
+                }
+            }
+            
+        }
+    }
+    if (control == 1){
+        if(listaLetrasCorrectas.length==validadordeCaracteres.length){
+            console.log("Ganaste");
+            vidas(pincel,8);
+            control=0;
+        }
+    }
 },false);
 
-function controladorJuego (){
-    control = true;
-};
+reiniciar.addEventListener("click",function(){
+    limpiarPantalla(pincel);
+    iniciar.classList.remove("invisible");
+    formulario.classList.remove("invisible");
+    listaLetrasCorrectas = [];
+    validadordeCaracteres = [];
+    palabraSecreta="";
+    guiones="";
+    control = 0;
+    contarmalo=1;
+    con=0;
+    cantidadMaxima= 0;
+});
+
+
 
 
 
--- js/teclado.js
+++ js/teclado.js
@@ -4,24 +4,21 @@
  * el canvas, la palabra para ver en la pantalla
  * y la ubicacion "x" y "y"
  */
-function dibujar(pincel,palabra,x,y){
-    pincel.fillStyle = "black";
-    pincel.font = "35px Arial";
+function dibujar(pincel,color,palabra,tamanoFamilia,x,y){
+    pincel.fillStyle = color;
+    pincel.font = tamanoFamilia;
     pincel.fillText(palabra,x,y);
 }
 
 function dato (imagen, pincel,x1,y1,a1,b1,x2,y2,a2,b2){
     imagen.addEventListener('load',() => {
         pincel.drawImage(imagen,x1,y1,a1,b1,x2,y2,a2,b2);
-     console.log("ya");
 });
 }
 
 function crearHomero(pincel,num,x1,y1,a1,b1,x2,y2,a2,b2){
     var vidaUno = document.createElement('img');
     vidaUno.src = 'media/Recurso'+num+'.png';
-    console.log(vidaUno);
-
     dato(vidaUno,pincel,x1,y1,a1,b1,x2,y2,a2,b2);
 }
 
@@ -31,7 +28,8 @@ function vidas (pincel,num){
         crearBaseHorca(pincel,100,10);
     }
     if (num==2){
-        crearHomero(pincel,num,0,0,350,350,350,120,150,150);
+        crearHomero(pincel,num,0,0,300,700,336,28,140,325);
+        crearHorca(pincel,100,10)
     }
     if (num==3){
         crearHomero(pincel,num,0,0,350,360,279,214,150,150);
@@ -48,20 +46,41 @@ function vidas (pincel,num){
     if (num==7){
         crearHomero(pincel,num,0,0,315,375,280,339,150,150);
     }
+    if (num==8){
+        crearHomero(pincel,num,0,0,2729,543,550,500,600,120);
+    }
+    if (num==9){
+        crearHomero(pincel,num,0,0,1404,333,600,500,466,111);
+    }
     
 
 }
+function crearHorca(pincel,x,y){
+    pincel.fillStyle = "black";
+    pincel.beginPath();
+    pincel.moveTo(80+x,20+y);
+    pincel.lineTo(110+x,50+y);
+    pincel.lineTo(340+x,50+y);
+    pincel.lineTo(340+x,20+y);
+    pincel.fill();
+};
+
 function crearBaseHorca(pincel,x,y){
     pincel.fillStyle = "black";
     pincel.beginPath();
-    pincel.moveTo(80+x,40+y);
+    pincel.moveTo(80+x,20+y);
     pincel.lineTo(80+x,600+y);
     pincel.lineTo(10+x,600+y);
     pincel.lineTo(10+x,630+y);
     pincel.lineTo(380+x,630+y);
     pincel.lineTo(380+x,600+y); // medio
     pincel.lineTo(110+x,600+y);
-    pincel.lineTo(110+x,70+y);
+    pincel.lineTo(110+x,50+y);
     pincel.fill();
 }
+function limpiarPantalla(pincel){
+    pincel.clearRect(0,0,1200,700);
+}
+
+
 
